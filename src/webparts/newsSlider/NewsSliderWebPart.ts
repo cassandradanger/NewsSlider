2de5fc2cab55f3c3acd5a4725176303dc1879865
Casie Siekman <casie.siekman@gmail.com>
2018-12-09 18:20:48 -0600

    initial commit

---
+++ src/webparts/newsSlider/NewsSliderWebPart.ts
@@ -0,0 +1,83 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './NewsSliderWebPart.module.scss';
+import * as strings from 'NewsSliderWebPartStrings';
+import {  
+  SPHttpClient  
+} from '@microsoft/sp-http';  
+
+export interface ISPLists {
+  value: ISPList[];  
+}
+
+export interface ISPList{
+}
+
+export interface INewsSliderWebPartProps {
+  description: string;
+}
+
+export default class NewsSliderWebPart extends BaseClientSideWebPart<INewsSliderWebPartProps> {
+
+  public render(): void {
+    this.domElement.innerHTML = `
+    <div class=${styles.mainNS}>
+       TESTING
+       <div id="spListContainer" /></div>
+    </div>`;
+      this._firstGetList();
+  }
+  private _firstGetList() {
+    this.context.spHttpClient.get('https://girlscoutsrv.sharepoint.com' + 
+      `/_api/web/Lists/GetByTitle('Stories, News, & Announcements')/Items?)`, SPHttpClient.configurations.v1)
+      .then((response)=>{
+        response.json().then((data)=>{
+          console.log('this is data', data);
+          this._renderList(data.value)
+        })
+      });
+  }
+
+  private _renderList(items: ISPList[]): void {
+    let html: string = ``;
+    items.forEach((item: ISPList) => {
+      html += ` 
+      <p>testing</p>
+      `
+    });
+    const listContainer: Element = this.domElement.querySelector('#spListContainer');  
+    listContainer.innerHTML = html;
+  };
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
